61eaa8c8de369f043566eb81631c7090948d6c51
iakotopoulos <i.a.kotopoulos@gmail.com>
2013-11-02 12:16:14 +0200

    Update DBConnection.java

--- src/com/ePark/data/connection/DBConnection.java
+++ src/com/ePark/data/connection/DBConnection.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.ePark.data.connection;
 
 import com.ePark.data.io.AppConfiguration;
