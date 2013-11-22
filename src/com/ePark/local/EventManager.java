42c67b5e5622cd447c40fc944588ddcba682bef2
iak <i.a.kotopoulos@gmail.com>
2013-11-22 12:20:07 +0200

    integration with new service parameters

--- src/com/ePark/local/EventManager.java
+++ src/com/ePark/local/EventManager.java
@@ -8,6 +8,7 @@ import com.ePark.data.io.AppConfiguration;
 import com.ePark.data.io.EparkIO;
 import com.ePark.http_json.HttpPoster;
 import com.ePark.http_json.MessageTypeException;
+import com.ePark.http_json.ParkingException;
 import com.ePark.local.events.DeviceListener;
 import com.ePark.local.rfid.ReaderManager;
 import com.ePark.local.rfid.SerialManager;
@@ -66,7 +67,7 @@ public class EventManager implements DeviceListener {
         try {
 
             //Supposing it is a magnetic at the entrance            
-            JSONObject arrival1 = httpPost.postArrival("IN", 1, "PK001", 1234567892, "123", "20130923210100", 123);
+            JSONObject arrival1 = httpPost.postArrival("IN", "1.0", "PK001", "1234567892", "123", "20130923210100", "123");
 //            System.out.println("ARRIVAL : " + arrival1.toString());
 
 
@@ -81,6 +82,8 @@ public class EventManager implements DeviceListener {
             Logger.getLogger(EventManager.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             messageSend = false;
+            Logger.getLogger(EventManager.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParkingException ex) {
             Logger.getLogger(EventManager.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
             EparkIO.storeArrival(theTagEvent, messageSend);
