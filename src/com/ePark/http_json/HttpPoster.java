812ac67954f3f018aff8afd4cfcd59e225d32e7d
ykoto <ykoto@PLATONAS2>
2013-12-01 10:36:00 +0200

    old commit with minor changes

--- src/com/ePark/http_json/HttpPoster.java
+++ src/com/ePark/http_json/HttpPoster.java
@@ -10,8 +10,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import net.sf.json.JSONObject;
 
 //import net.sf.json.JSONException;
