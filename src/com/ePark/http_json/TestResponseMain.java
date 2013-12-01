a4325b78fdcbd7946c8e151b013ce709add44b33
iak <i.a.kotopoulos@gmail.com>
2013-12-01 10:24:40 +0200

    trip

--- src/com/ePark/http_json/TestResponseMain.java
+++ src/com/ePark/http_json/TestResponseMain.java
@@ -9,7 +9,6 @@ import java.net.MalformedURLException;
 import java.net.SocketTimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import net.sf.json.JSONObject;
 
 /**
  *
